cd2e347059abb392d5ceb038a6a164fc1ae95c38
mooveslikediegstan <diego.stanzani1@hotmail.com>
2026-07-08 22:20:49 -0300

    fix: uncomment procedure execution file

--- 2_sql_scripts/10_load_procedures_execution.sql
+++ 2_sql_scripts/10_load_procedures_execution.sql
@@ -2,12 +2,12 @@
 -- 10. EXECUTE LOADING PROCEDURES (AFTER IMPORTING DATA)
 -- ============================================================================
  
---EXEC [dw].[sp_Load_Dimension_Customers];
---EXEC [dw].[sp_Load_Dimension_Products];
---EXEC [dw].[sp_Load_Dimension_Sellers];
---GO
---EXEC [dw].[sp_Load_Fact_Sales];
---EXEC [dw].[sp_Load_Fact_Reviews];
+EXEC [dw].[sp_Load_Dimension_Customers];
+EXEC [dw].[sp_Load_Dimension_Products];
+EXEC [dw].[sp_Load_Dimension_Sellers];
+GO
+EXEC [dw].[sp_Load_Fact_Sales];
+EXEC [dw].[sp_Load_Fact_Reviews];
 EXEC [dw].[sp_Load_Fact_Payments]; 
 GO
  
